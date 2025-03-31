b98d483947015212d6f6d93b787608f48ee63dbd
sushruth chunchu <sushruth.ch@shitscared.local>
2025-03-31 11:22:07 -0400

    Xpath expressions

--- main.py
+++ main.py
@@ -15,5 +15,12 @@ from pathlib import Path
 # Now we'll define some paths to store for us to access later
 root_address = Path("/Users/sushruth.ch/Desktop/NDC Scraper/Unzipped data")
 root_image_address =Path("Images")
-root_image_address.mkdir(exist_ok=True)
+root_image_address.mkdir(exist_ok=True) # this is to create root_image_address if the file didnt ever exists
 
+data_row=[] # this helps us to store the data we extractsfrom the XML files 
+
+xml_files= list(root_address.rglob("*.xml"))
+print(f"Found {len(xml_files)} in the given directory")
+
+for xml_file in xml_files:
+    
